799f0ee5d38f434cfd2cc1e2f1fe6be2e342642f
Clay Smalley <claysmalley@gmail.com>
2012-11-23 02:31:55 -0600

    seriously? whitespace matters? sh i am disappoint

--- csconnect.sh
+++ csconnect.sh
@@ -1,10 +1,10 @@
 # Change this to your UT CS username.
-username = "clay"
+username="clay"
 
 # Change this to 32 if you would rather connect to a 32-bit machine.
 # Or set it to $1 to make the first argument the bits, e.g.:
 # 	$ csconnect.sh 32
 # 	$ csconnect.sh 64
-bits = 64
+bits=64
 
 ssh -X $username@`cshosts.py $bits`
